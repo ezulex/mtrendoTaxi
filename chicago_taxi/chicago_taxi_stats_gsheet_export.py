5fcc23f81cecbbcad0802bc7989036b89e71adbe
ezulex <bochik1989@mail.ru>
2024-07-21 17:17:30 +0400

    Add sheet_ID const

--- chicago_taxi/chicago_taxi_stats_gsheet_export.py
+++ chicago_taxi/chicago_taxi_stats_gsheet_export.py
@@ -10,6 +10,7 @@ SERVICE_ACCOUNT_FILE = os.environ.get("SERVICE_ACCOUNT_FILE")
 PROJECT_ID = os.environ.get("PROJECT_ID")
 DATASET_ID = os.environ.get("DATASET_ID")
 TABLE_ID = os.environ.get("TABLE_ID")
+SPREADSHEET_ID = os.environ.get("SPREADSHEET_ID")
 
 client = bigquery.Client.from_service_account_json(SERVICE_ACCOUNT_FILE)
 
@@ -24,7 +25,7 @@ scopes = ['https://www.googleapis.com/auth/spreadsheets']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, scopes)
 gc = gspread.authorize(credentials)
 
-SPREADSHEET_ID = '1tCunA1i86ZlUgWIDN5gpl8f4Rt44lVfsa4pq2PYAgu0'
+
 sh = gc.open_by_key(SPREADSHEET_ID)
 
 worksheet = sh.get_worksheet(0)
@@ -33,4 +34,4 @@ worksheet.clear()
 
 worksheet.update([df.columns.values.tolist()] + df.values.tolist())
 
-print("Данные успешно загружены")
+print("Data successfully exported")
